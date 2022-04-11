82c1c85b134543fdc7135d88052da7251eac76c4
Sami-Q24 <98277017+Sami-Q24@users.noreply.github.com>
2022-04-11 11:37:46 +0300

    Update game5.py

--- game5.py
+++ game5.py
@@ -39,15 +39,39 @@ def ship_right():
     y = y + 15
     ship.setx(y)
 
+<<<<<<< Updated upstream
 def border():
     if ship.xcor()>200:
         ship.setx(200)
     elif ship.xcor()<-200:
         ship.setx(-200)
+=======
+def ship_down():
+    x = ship.ycor()
+    x = x - 15
+    ship.sety(x)
+
+def ship_up():
+    x = ship.ycor()
+    x = x + 15
+    ship.sety(x)
+
+def ship_up_right():
+    x = ship.ycor()
+    y = ship.xcor()
+    x = x+15
+    y = y+15
+    ship.setx(y)
+    ship.sety(x)
+
+>>>>>>> Stashed changes
 
 window.listen()
 window.onkeypress(ship_left, "a")
 window.onkeypress(ship_right, "d")
+window.onkeypress(ship_up, "w")
+window.onkeypress(ship_down, "s")
+window.onkeypress(ship_up_right, "w" and "d")
 
 while True:
     window.update()
