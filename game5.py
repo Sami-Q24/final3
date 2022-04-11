cd1f75a4217d92855f7421f980fb7792b6f5a3a5
Sami-Q24 <98277017+Sami-Q24@users.noreply.github.com>
2022-04-11 11:26:27 +0300

    Astroid movement

--- game5.py
+++ game5.py
@@ -1,4 +1,5 @@
 import turtle
+import time
 
 window = turtle.Screen()
 window.setup(width=1000, height=400)
@@ -12,12 +13,18 @@ ship.shape('square')
 ship.goto(-450, 0)
 ship.color('red')
 
+vel = -1
+
 def create_astroids():
     astroid_ = turtle.Turtle()
-    astroid_.goto(0, 0)
+    astroid_.penup()
+    astroid_.goto(500, 0)
     astroid_.color("yellow")
     astroid_.shape("square")
-    astroid_.shapesize(stretch_wid=5, stretch_len=5)
+    astroid_.shapesize(stretch_wid=2, stretch_len=2)
+    for i in range(1000):
+        window.update()
+        astroid_.setx(astroid_.xcor()+vel)
 
 create_astroids()
 
