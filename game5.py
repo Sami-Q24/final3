24fd6d7ca916f9629b387dfb99fc7f57eca3f6af
Sami-Q24 <98277017+Sami-Q24@users.noreply.github.com>
2022-04-20 11:50:05 +0300

    Update game5.py

--- game5.py
+++ game5.py
@@ -2,6 +2,13 @@ import random
 import turtle
 import time
 
+
+window = turtle.Screen()
+window.setup(width=1000, height=400)
+window.bgcolor('black')
+window.tracer(0)
+
+
 ship = turtle.Turtle()
 ship.speed(0)
 ship.penup()
@@ -15,6 +22,7 @@ window.setup(width=1000, height=400)
 window.bgcolor('black')
 window.tracer(0)
 
+<<<<<<< Updated upstream
 
 def create_astroids(spawny, steps):
     bullet = turtle.Turtle()
@@ -54,8 +62,11 @@ def create_astroids(spawny, steps):
     astroid_.hideturtle()
     turtle.hideturtle()
 
+=======
+>>>>>>> Stashed changes
 def hide():
     ship.hideturtle()
+
 
 def ship_left():
     y = ship.xcor()
@@ -74,14 +85,12 @@ def ship_down():
      y = ship.ycor()
      y = y - 15
      ship.sety(y)
-def ship_up_right():
-    x = ship.ycor()
-    y = ship.xcor()
-    x = x+15
-    y = y+15
-    ship.setx(y)
-    ship.sety(x)
 
+<<<<<<< Updated upstream
+=======
+vel = -3/4
+
+>>>>>>> Stashed changes
 def border():
     if ship.xcor()>480:
         ship.setx(480)
@@ -93,6 +102,7 @@ def border():
         ship.sety(-175)
 
 
+<<<<<<< Updated upstream
 vel = -3/4
 
 window.listen()
@@ -100,6 +110,23 @@ window.onkeypress(ship_left, "a")
 window.onkeypress(ship_right, "d")
 window.onkeypress(ship_up, "w")
 window.onkeypress(ship_down, "s")
+=======
+def create_astroids(spawny, steps):
+    astroid_ = turtle.Turtle()
+    astroid_.penup()
+    astroid_.goto(500, spawny)
+    astroid_.color("yellow")
+    astroid_.shape("square")
+    astroid_.shapesize(stretch_wid=2, stretch_len=2)
+    for i in range(steps):
+        window.update()
+        border()
+        if (ship.xcor() + 10>astroid_.xcor()-20 and ship.xcor()+10 < astroid_.xcor()+20 and ship.ycor() > astroid_.ycor()-20 and ship.ycor() < astroid_.ycor()+20):
+            hide()
+
+        astroid_.setx(astroid_.xcor()+vel)
+    astroid_.hideturtle()
+>>>>>>> Stashed changes
 
 for i in range(100):
     window.update()
@@ -107,11 +134,57 @@ for i in range(100):
     vel = random.randint(2, 7)
     vel = -(vel/5)
     vel_steps = -vel
+<<<<<<< Updated upstream
     steps = int((1000//vel_steps) + 10)
+=======
+    steps = int((1000//vel_steps) + 2)
+>>>>>>> Stashed changes
     create_astroids(spawny, steps)
+
+
+bulletx=3
+bullet_despawn = False
+def fire():
+    bullet_despawn = True
+    print("shot")
+    bullet = turtle.Turtle()
+    bullet.setx(ship.xcor())
+    bullet.sety(ship.ycor())
+    bullet.speed(0)
+    bullet.penup()
+    bullet.shape('circle')
+    bullet.color('blue')
+    bullet.shapesize(0.5)
+    while bullet_despawn == True:
+        if (bullet.xcor()<1010):
+            window.update()
+            bullet.setx(bullet.xcor() + bulletx)
+        else:
+            bullet.hideturtle()
+            bullet_despawn = False
+            return
+
+
+
+window.listen()
+window.onkeypress(ship_left, "a")
+window.onkeypress(ship_right, "d")
+window.onkeypress(ship_up, "w")
+window.onkeypress(ship_down, "s")
+if bullet_despawn == False:
+    window.onkeypress(fire, "u")
+
+
+def fire_bullet():
+   x = ship.xcor()
+   y = ship.ycor()
+   bullet.setx(x)
+   bullet.sety(y)
+   if window.onkeypress(fire_bullet, "u"):
+       bullet.sety(bullet.ycor() + bullety)
+
 
 
 while True:
     window.update()
     border()
-
