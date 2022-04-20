6949aea99ce09010b4cce103cd7edd16d8399cee
Sami-Q24 <98277017+Sami-Q24@users.noreply.github.com>
2022-04-20 11:49:32 +0300

    Update game5.py

--- game5.py
+++ game5.py
@@ -10,24 +10,49 @@ ship.shapesize(stretch_wid=1, stretch_len=1)
 ship.goto(-450, 0)
 ship.color('red')
 
+window = turtle.Screen()
+window.setup(width=1000, height=400)
+window.bgcolor('black')
+window.tracer(0)
 
-bullet = turtle.Turtle()
-bullet.speed(0)
-bullet.penup()
-bullet.shape('circle')
-bullet.color('blue')
-bullet.shapesize(0.5)
-bullety=1.5
 
-def fire_bullet():
-   x = ship.xcor()
-   y = ship.ycor()
-   bullet.setx(x)
-   bullet.sety(y)
-   if window.onkeypress(fire_bullet, "u"):
-       bullet.sety(bullet.ycor() + bullety)
-
-fire_bullet()
+def create_astroids(spawny, steps):
+    bullet = turtle.Turtle()
+    bullet.speed(0)
+    bullet.penup()
+    bullet.shape('circle')
+    bullet.color('blue')
+    bullet.shapesize(0.5)
+    bullety = 2
+    bullet.hideturtle()
+    global shot
+    shot = False
+    def firebullet():
+        bullet.sety(ship.ycor())
+        bullet.setx(ship.xcor())
+        bullet.showturtle()
+        global shot
+        shot = True
+        print("trigger1")
+    window.onkeypress(firebullet, "e")
+    astroid_ = turtle.Turtle()
+    astroid_.penup()
+    astroid_.goto(500, spawny)
+    astroid_.color("yellow")
+    astroid_.shape("square")
+    astroid_.shapesize(stretch_wid=2, stretch_len=2)
+    for i in range(steps):
+        window.update()
+        if (ship.xcor() + 10>astroid_.xcor()-20 and ship.xcor()+10 < astroid_.xcor()+20 and ship.ycor() > astroid_.ycor()-20 and ship.ycor() < astroid_.ycor()+20):
+            hide()
+        border()
+        if shot == True:
+            print("trigger2")
+            bullet.setx(bullet.xcor() + bullety)
+        astroid_.setx(astroid_.xcor()+vel)
+    shot = False
+    astroid_.hideturtle()
+    turtle.hideturtle()
 
 def hide():
     ship.hideturtle()
@@ -57,30 +82,6 @@ def ship_up_right():
     ship.setx(y)
     ship.sety(x)
 
-vel = -3/4
-
-
-window = turtle.Screen()
-window.setup(width=1000, height=400)
-window.bgcolor('black')
-window.tracer(0)
-
-
-window.listen()
-window.onkeypress(ship_left, "a")
-window.onkeypress(ship_right, "d")
-window.onkeypress(ship_up, "w")
-window.onkeypress(ship_down, "s")
-
-
-bullet = turtle.Turtle()
-bullet.speed(0)
-bullet.penup()
-bullet.shape('circle')
-bullet.color('blue')
-bullet.shapesize(5)
-bullety=1.5
-
 def border():
     if ship.xcor()>480:
         ship.setx(480)
@@ -91,36 +92,14 @@ def border():
     elif ship.ycor()<-175:
         ship.sety(-175)
 
+
+vel = -3/4
+
 window.listen()
 window.onkeypress(ship_left, "a")
 window.onkeypress(ship_right, "d")
 window.onkeypress(ship_up, "w")
 window.onkeypress(ship_down, "s")
-
-def fire_bullet():
-   x = ship.xcor()
-   y = ship.ycor()
-   bullet.setx(x)
-   bullet.sety(y)
-   if window.onkeypress(fire_bullet, "u"):
-       bullet.sety(bullet.ycor() + bullety)
-
-
-def create_astroids(spawny, steps):
-    astroid_ = turtle.Turtle()
-    astroid_.penup()
-    astroid_.goto(500, spawny)
-    astroid_.color("yellow")
-    astroid_.shape("square")
-    astroid_.shapesize(stretch_wid=2, stretch_len=2)
-    for i in range(steps):
-        window.update()
-        if (ship.xcor() + 10>astroid_.xcor()-20 and ship.xcor()+10 < astroid_.xcor()+20 and ship.ycor() > astroid_.ycor()-20 and ship.ycor() < astroid_.ycor()+20):
-            hide()
-        border()
-
-        astroid_.setx(astroid_.xcor()+vel)
-    astroid_.hideturtle()
 
 for i in range(100):
     window.update()
@@ -128,9 +107,7 @@ for i in range(100):
     vel = random.randint(2, 7)
     vel = -(vel/5)
     vel_steps = -vel
-    print(vel)
-    print("steps" + str(100//vel_steps))
-    steps = int((1000//vel_steps) + 2)
+    steps = int((1000//vel_steps) + 10)
     create_astroids(spawny, steps)
 
 
